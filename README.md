8eb8c1603b169e256a0e2aa77b2e55522eb0d266
Marie Dillard <mariedillard@protonmail.com>
2019-01-31 14:45:56 -0800

    adds full readme and redoes code so computer is more competent. streamlines process and adds styling to all available pages.

--- README.md
+++ README.md
@@ -1,6 +1,41 @@
+# _Pig Dice_
+
+#### _A pig dice game, January/31/2019_
+
+#### By _**Marie @Milkybot**_
+
+## Description
+
+A website that allows you to play a virtual game of pig dice! To view on github pages, follow the link: https://milkybot.github.io/pig-dice/
+
+## Specifications
 | Description | Input | Output |
 | ----------- | ----- | ------ |
-| Allows player to roll a six sided virtual dice. | "*Clicks roll button.*" | "5!" |
+| Allows player to roll a six sided virtual dice. | "*Clicks roll button.*" | "*Returns a number*" |
 | If the player rolls a one, they earn no points and they end their turn. | "*Rolls a One.*" | "Snake eyes!" |
-| If the player rolls any other number, it is added to their score. | "*Rolls a five.*" | "Five points!" |
+| If the player rolls any other number, it is added to their score. | "*Rolls a five.*" | "So far you've scored: 5" |
 | If a player chooses to hold, their turn total is added and they end their turn. | "*Clicks hold.*" | "Player Two's turn!" |
+
+## _Setup/Installation Requirements_
+
+* _Requires Bootstrap.css (located in css folder)_
+* _Requires Jquery-3.3.1.js (located in js folder)_
+* _Open terminal, navigate onto desktop using the command "cd Desktop"_
+* _Perform git clone using link provided by git hub and entering "git clone" in terminal._
+* _After download, open folder and click index.html._
+
+
+## Known Bugs
+
+_No known bugs! Please use relevant contact details to report any._
+
+## Support and contact details
+
+_Contact Marie @mariedillard on github and mariedillard@protonmail.com for support, bugs, questions, and comments._
+
+## Technologies Used
+
+_Created using atom, code relies and uses elements from jquery-3.1.1 and bootstrap css. Questionnaire and javascript elements rely heavily on jquery for the logic, while html and css relies heavily on bootstrap for proper formatting and browser compatibility._
+
+### License
+Copyright (c) 2018 **milkybot**
--- js/scripts1vComputer.js
+++ js/scripts1vComputer.js
@@ -1,4 +1,3 @@
-var score = 0;
 var value = 1;
 
 function getRandomInt(min, max) {
@@ -7,173 +6,41 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) +min;
 }
 
-var playerOne = {
-  score: 0,
-  endScore: 0,
+function scoreKeeper(score, endScore){
+  this.score = score,
+  this.endScore = endScore
 }
 
-var playerComputer = {
-  score: 0,
-  endScore: 0,
+function gameAlert(computer, player){
+  this.computer = computer,
+  this.player = player
 }
 
-function healthbar(){
-  if (playerComputer.endScore - playerOne.endScore <= -30){
-    $("#pos30Pig").hide()
-    $("#pos25Pig").hide()
-    $("#pos20Pig").hide()
-    $("#pos15pig").hide()
-    $("#neg15Pig").hide()
-    $("#neg20Pig").hide()
-    $("#neg25Pig").hide()
-    $("#neg30Pig").show()
+var playerOne = new scoreKeeper(0, 0);
+var playerComputer = new scoreKeeper(0, 0);
+var currentGame = new gameAlert(" ", " ");
 
-    $("#pos30Rab").hide()
-    $("#pos25Rab").hide()
-    $("#pos20Rab").hide()
-    $("#pos15Rab").hide()
-    $("#neg15Rab").hide()
-    $("#neg20Rab").hide()
-    $("#neg25Rab").hide()
-    $("#neg30Rab").show()
-
-    //Computer : I'ts not fair wahh!!
-  } else if(playerComputer.endScore - playerOne.endScore <= -25){
-    $("#pos30Pig").hide()
-    $("#pos25Pig").hide()
-    $("#pos20Pig").hide()
-    $("#pos15pig").hide()
-    $("#neg15Pig").hide()
-    $("#neg20Pig").hide()
-    $("#neg25Pig").show()
-    $("#neg30Pig").hide()
-
-    $("#pos30Rab").hide()
-    $("#pos25Rab").hide()
-    $("#pos20Rab").hide()
-    $("#pos15Rab").hide()
-    $("#neg15Rab").hide()
-    $("#neg20Rab").hide()
-    $("#neg25Rab").show()
-    $("#neg30Rab").hide()
-
-    //Computer: Your uh, pretty good.
-  } else if(playerComputer.endScore - playerOne.endScore <= -20){
-    $("#pos30Pig").hide()
-    $("#pos25Pig").hide()
-    $("#pos20Pig").hide()
-    $("#pos15pig").hide()
-    $("#neg15Pig").hide()
-    $("#neg20Pig").show()
-    $("#neg25Pig").hide()
-    $("#neg30Pig").hide()
-
-    $("#pos30Rab").hide()
-    $("#pos25Rab").hide()
-    $("#pos20Rab").hide()
-    $("#pos15Rab").hide()
-    $("#neg15Rab").hide()
-    $("#neg20Rab").show()
-    $("#neg25Rab").hide()
-    $("#neg30Rab").hide()
-
-    //Computer: Youre not that good!!
-  } else if(playerComputer.endScore - playerOne.endScore <= -15){
-    $("#pos30Pig").hide()
-    $("#pos25Pig").hide()
-    $("#pos20Pig").hide()
-    $("#pos15pig").hide()
-    $("#neg15Pig").show()
-    $("#neg20Pig").hide()
-    $("#neg25Pig").hide()
-    $("#neg30Pig").hide()
-
-    $("#pos30Rab").hide()
-    $("#pos25Rab").hide()
-    $("#pos20Rab").hide()
-    $("#pos15Rab").hide()
-    $("#neg15Rab").show()
-    $("#neg20Rab").hide()
-    $("#neg25Rab").hide()
-    $("#neg30Rab").hide()
-
-    //Computer: Hmmpf!
-  } else if(playerComputer.endScore - playerOne.endScore <= 15){
-    $("#pos30Pig").hide()
-    $("#pos25Pig").hide()
-    $("#pos20Pig").hide()
-    $("#pos15pig").show()
-    $("#neg15Pig").hide()
-    $("#neg20Pig").hide()
-    $("#neg25Pig").hide()
-    $("#neg30Pig").hide()
-
-    $("#pos30Rab").hide()
-    $("#pos25Rab").hide()
-    $("#pos20Rab").hide()
-    $("#pos15Rab").show()
-    $("#neg15Rab").hide()
-    $("#neg20Rab").hide()
-    $("#neg25Rab").hide()
-    $("#neg30Rab").hide()
-
-  }else if(playerComputer.endScore - playerOne.endScore <= 20){
-    $("#pos30Pig").hide()
-    $("#pos25Pig").hide()
-    $("#pos20Pig").show()
-    $("#pos15pig").hide()
-    $("#neg15Pig").hide()
-    $("#neg20Pig").hide()
-    $("#neg25Pig").hide()
-    $("#neg30Pig").hide()
-
-    $("#pos30Rab").hide()
-    $("#pos25Rab").hide()
-    $("#pos20Rab").show()
-    $("#pos15Rab").hide()
-    $("#neg15Rab").hide()
-    $("#neg20Rab").hide()
-    $("#neg25Rab").hide()
-    $("#neg30Rab").hide()
-    //Computer: Hehehe,
-  } else if(playerComputer.endScore - playerOne.endScore <= 25){
-    $("#pos30Pig").hide()
-    $("#pos25Pig").show()
-    $("#pos20Pig").hide()
-    $("#pos15pig").hide()
-    $("#neg15Pig").hide()
-    $("#neg20Pig").hide()
-    $("#neg25Pig").hide()
-    $("#neg30Pig").hide()
-
-    $("#pos30Rab").hide()
-    $("#pos25Rab").show()
-    $("#pos20Rab").hide()
-    $("#pos15Rab").hide()
-    $("#neg15Rab").hide()
-    $("#neg20Rab").hide()
-    $("#neg25Rab").hide()
-    $("#neg30Rab").hide()
-    //Computer: Remember not to be a sore loser when I beat you.
-  } else if(playerComputer.endScore - playerOne.endScore <= 30) {
-    $("#pos30Pig").show()
-    $("#pos25Pig").hide()
-    $("#pos20Pig").hide()
-    $("#pos15pig").hide()
-    $("#neg15Pig").hide()
-    $("#neg20Pig").hide()
-    $("#neg25Pig").hide()
-    $("#neg30Pig").hide()
-
-    $("#pos30Rab").show()
-    $("#pos25Rab").hide()
-    $("#pos20Rab").hide()
-    $("#pos15Rab").hide()
-    $("#neg15Rab").hide()
-    $("#neg20Rab").hide()
-    $("#neg25Rab").hide()
-    $("#neg30Rab").hide()
-    //Computer: GIT GUD!!
+function competitionPig(){
+  var competitiveScore = playerOne.endScore - playerComputer.endScore;
+  if (playerComputer.endScore >= 100){
+    document.getElementById("competitionTxt").innerHTML = "Loser!";
+    document.getElementById("competitionPig").innerHTML = "<img src='img/loser.gif'>";
+    document.getElementById("newGame").innerHTML = "Play again?";
+  }else if (competitiveScore <= 0){
+    document.getElementById("competitionTxt").innerHTML = "You aren't very good at this, are you?";
+    document.getElementById("competitionPig").innerHTML = "<img src='img/1.gif'>";
+  } else if (competitiveScore <= 10){
+    document.getElementById("competitionTxt").innerHTML = "You aren't that far ahead!";
+    document.getElementById("competitionPig").innerHTML = "<img src='img/2.gif'>";
+  } else if (competitiveScore <= 20){
+    document.getElementById("competitionTxt").innerHTML = "Hey, slow down!";
+    document.getElementById("competitionPig").innerHTML = "<img src='img/3.gif'>";
+  } else if (competitiveScore <= 30){
+    document.getElementById("competitionTxt").innerHTML = "That's not good!";
+    document.getElementById("competitionPig").innerHTML = "<img src='img/4.gif'>";
+  } else {
+    document.getElementById("competitionTxt").innerHTML = "!!!!!!!!!!";
+    document.getElementById("competitionPig").innerHTML = "<img src='img/5.gif'>";
   }
 }
 
@@ -181,52 +48,67 @@ function computerTurn(){
   while(value === 2){
     var comDiceRoll = getRandomInt(1, 6);
     var computerChoice = getRandomInt(1, 100);
-
+    playerComputer.score = 0;
     if(comDiceRoll === 1){
-      alert("SNAKE EYES!");
-      alert("Computer ends their turn!")
-      playerComputer.score = 0
-      value -=  1
+      currentGame.computer = "SNAKE EYES! +0!";
+      playerComputer.score = 0;
+      value -=  1;
     } else if(comDiceRoll <= 6){
-      playerComputer.score += comDiceRoll
-        if(computerChoice >= 50){
+      playerComputer.score += comDiceRoll+2;
+        if(computerChoice >= 25){
           playerComputer.endScore += playerComputer.score
           document.getElementById("comScore").innerHTML = playerComputer.endScore;
-            if (playerComputer.endScore >= 100 || playerComputer.endScore === 100) {
-              alert("COMPUTER WINS!");
-            } else {
-              alert("Computer ends their turn!");
-            }
-          value -= 1
-      } else{
-
+          if (playerComputer.endScore >= 100 || playerComputer.endScore === 100) {
+            currentGame.computer = "COMPUTER WINS!";
+            currentGame.player = " ";
+            $("p#buttons").empty();
+          } else {
+            currentGame.computer = "Computer scored: " + playerComputer.score;
+          }
+        value -= 1;
       }
     }
+    competitionPig();
+    document.getElementById("computerAlert").innerHTML = currentGame.computer;
   }
 }
 
 $(document).ready(function(){
   $("button#pl1rollDice").click(function(event){
+    event.preventDefault();
     var randomRollDice = getRandomInt(1,6)
     if (randomRollDice <= 1){
       playerOne.score = 0;
-      alert("Snake eyes!");
-      alert("Player One ends their turn!");
+      currentGame.player = "SNAKE EYES! +0!";
       value += 1
       computerTurn()
     } else if (randomRollDice > 1 && randomRollDice <= 6) {
-      playerOne.score += randomRollDice
+      playerOne.score += randomRollDice;
+      currentGame.player = "So far you've scored: " + playerOne.score;
     }
+    document.getElementById("playerAlert").innerHTML = currentGame.player;
   });
   $("button#pl1Hold").click(function(event){
+    event.preventDefault();
       playerOne.endScore += playerOne.score
       document.getElementById("pl1Score").innerHTML = playerOne.endScore;
       if (playerOne.endScore >= 100 || playerOne.endScore === 100) {
-          alert("PLAYER ONE WINS!");
+        currentGame.player = "PLAYER ONE WINS!";
+        currentGame.computer = " ";
+        document.getElementById("competitionTxt").innerHTML = " ";
+        document.getElementById("competitionPig").innerHTML = "<img src='img/winner.gif'>";
+        $("p#buttons").empty();
+        document.getElementById("newGame").innerHTML = "Play again?";
       } else {
-        alert("Computer's turn!");
-        value += 1
-        computerTurn()
+        value += 1;
+        currentGame.player = "Keep going!";
+        computerTurn();
       }
+      playerOne.score = 0;
+      document.getElementById("playerAlert").innerHTML = currentGame.player;
+    });
+    $("button#newGame").click(function(event){
+      event.preventDefault();
+      window.location.reload();
     });
 });
